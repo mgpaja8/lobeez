2c501758a977569cbb4ca08c06f6e145dae12a7a
Pavle Milicevic <pavle.milicevic@brandingbrand.com>
2019-01-28 21:48:14 -0500

    chore: Config client
    Used create-react-app to generate boilerplate

---
+++ client/src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Empty App
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
